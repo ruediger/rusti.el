2e64b456fa0fc2bc72ad0db6c76dede13835c14d
Rüdiger Sonderfeld <ruediger@c-plusplus.de>
2013-08-10 13:52:51 +0200

    Initial.
    Signed-off-by: Rüdiger Sonderfeld <ruediger@c-plusplus.de>

---
+++ rusti.el
@@ -0,0 +1,100 @@
+;;; rusti --- Rust interactive mode -*- lexical-binding: t -*-
+
+;; Copyright (C) 2013 Rüdiger Sonderfeld
+
+;; Author: Rüdiger Sonderfeld <ruediger@c-plusplus.de>
+;; Created: 10 Aug 2013
+;; Keywords: rust languages
+;; URL: https://github.com/ruediger/rusti.el
+
+;;; Commentary:
+
+;; Run the rust REPL (rusti) in comint mode.
+
+;;; Code:
+
+(require 'comint)
+
+(defgroup rusti nil
+  "Rust interactive mode"
+  :link '(url-link "https://github.com/ruediger/rusti-mode")
+  :prefix "rusti-"
+  :group 'languages)
+
+(defcustom rusti-program (executable-find "rusti")
+  "Program invoked by `rusti'."
+  :group 'rusti
+  :type 'file)
+
+(defcustom rusti-args nil
+  "Command line arguments for `rusti-program'."
+  :group 'rusti
+  :type '(repeat string))
+
+(defcustom rusti-buffer "*Rusti*"
+  "Name of buffer for rusti."
+  :group 'rusti
+  :type 'string)
+
+(defcustom rusti-startup-file (locate-user-emacs-file "init_rusti.rs"
+                                                      ".emacs-rusti.rs")
+  "Startup file for `rusti'."
+  :group 'rusti
+  :type 'file)
+
+(defcustom rusti-prompt-regexp "^rusti> "
+  "Regexp to match prompts for rusti."
+  :group 'rusti
+  :type 'regexp)
+
+(defcustom rusti-prompt-read-only t
+  "Make the prompt read only.
+See `comint-prompt-read-only' for details."
+  :group 'rusti
+  :type 'boolean)
+
+(defun rusti-is-running? ()
+  "Return non-nil if rusti is running."
+  (comint-check-proc rusti-buffer))
+(defalias 'rusti-is-running-p #'rusti-is-running?)
+
+;;;###autoload
+(defun rusti (&optional arg)
+  "Run rusti.
+
+Unless ARG is non-nil, switch to the buffer."
+  (interactive "P")
+  (let ((buffer (get-buffer-create rusti-buffer)))
+    (unless arg
+      (pop-to-buffer buffer))
+    (unless (rusti-is-running?)
+      (with-current-buffer buffer
+        (rusti-startup)
+        (rusti-mode)))
+    buffer))
+
+;;;###autoload
+(defalias 'run-rust #'rusti)
+;;;###autoload
+(defalias 'inferior-rust #'rusti)
+
+(defun rusti-startup ()
+  "Start rusti."
+  (comint-exec rusti-buffer
+               "Rusti"
+               rusti-program
+               (when (file-exists-p rusti-startup-file)
+                 rusti-startup-file)
+               rusti-args))
+
+(define-derived-mode rusti-mode comint-mode "Rusti"
+  "Major mode for rusti."
+  (setq comint-prompt-regexp rusti-prompt-regexp
+        comint-use-prompt-regexp t)
+  (setq-local comment-start "// ")
+  (setq-local comment-end "")
+  (setq-local comint-prompt-read-only rusti-prompt-read-only))
+
+(provide 'rusti)
+
+;;; rusti.el ends here
